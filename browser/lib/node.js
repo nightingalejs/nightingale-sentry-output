b50790fc0af7bc3069ff132069b6837d969150e2
Christophe Hurpeau <christophe@hurpeau.com>
2016-04-27 21:29:43 +0200

    first commit

---
+++ browser/lib/node.js
@@ -0,0 +1,51 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _extends = Object.assign || /**
+                                 * @function
+                                 * @param target
+                                */ function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+exports.default = sentryOutput;
+
+var _raven = require('raven');
+
+/**
+ * @function
+ * @param ravenUrl
+*/function sentryOutput(ravenUrl) {
+    var ravenClient = new _raven.Client(ravenUrl);
+
+    return (/**
+            * @function
+            * @param _
+            * @param
+           */function write(_, _ref) {
+            var metadata = _ref.metadata;
+            var extra = _ref.extra;
+
+            var error = metadata.error;
+
+            if (!error) {
+                return;
+            }
+
+            var extraData = _extends({}, metadata, { extra: extra });
+            delete extraData.error;
+
+            if (error.originalError) {
+                // error-processor
+                extraData.parsedStack = error.stackTrace.toArray();
+                error = error.originalError;
+            }
+
+            ravenClient.captureError(error, {
+                extra: extraData
+            });
+        }
+    );
+}
+//# sourceMappingURL=node.js.map
---
+++ src/node.js
@@ -0,0 +1,41 @@
+import { Client as RavenClient } from 'raven';
+import levels from 'nightingale-levels';
+
+const mapToSentryLevel = {
+    [levels.TRACE]: 'debug',
+    [levels.DEBUG]: 'debug',
+    [levels.INFO]: 'info',
+    [levels.WARNING]: 'warning',
+    [levels.ERROR]: 'error',
+    [levels.FATAL]: 'fatal',
+    [levels.EMERGENCY]: 'fatal',
+}
+
+export default function sentryOutput(ravenUrl) {
+    const ravenClient = new RavenClient(ravenUrl);
+
+    return function write(_, { level, metadata, extra }) {
+        let error = metadata.error;
+
+        if (!error) {
+            return;
+        }
+
+        const extraData = { ...metadata, extra };
+        delete extraData.error;
+
+        if (error.originalError) {
+            // error-processor
+            extraData.parsedStack = error.stackTrace.toArray();
+            error = error.originalError;
+        }
+
+        ravenClient.captureError(
+            error,
+            {
+                level: mapToSentryLevel[level] || 'error',
+                extra: extraData,
+            }
+        );
+    };
+}
